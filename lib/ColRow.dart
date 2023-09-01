57b628c7919f8ff76c741eb04553e407d6bab54e
rumahrudi <rudy2001hariadi@gmail.com>
2023-09-01 22:33:57 +0700

    first commit

---
+++ lib/ColRow.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(MyAppColRow());
+// }
+
+class MyAppColRow extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Demo Row Column'),
+      ),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.green, // Warna latar belakang
+                      //foregroundColor: Colors.white,
+                    ),
+                    child: Text('Button 1'),
+                  ),
+                  SizedBox(height: 10),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                      //foregroundColor: Colors.white,
+                    ),
+                    child: Text('Button 2'),
+                  ),
+                  // SizedBox(width: 10),
+                ]),
+            Container(width: 10.0),
+            Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.red,
+                      //foregroundColor: Colors.white,
+                    ),
+                    child: Text('Button 3'),
+                  ),
+                  SizedBox(height: 10),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.purple,
+                      //foregroundColor: Colors.white,
+                    ),
+                    child: Text('Button 4'),
+                  ),
+                  // SizedBox(width: 10),
+                ]),
+            Container(width: 10.0),
+            Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.yellow,
+                      //foregroundColor: Colors.white,
+                    ),
+                    child: Text('Button 5'),
+                  ),
+                  SizedBox(height: 10),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.black,
+                      //foregroundColor: Colors.white,
+                    ),
+                    child: Text('Button 6'),
+                  ),
+                ])
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/Row.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(MyAppRow());
+// }
+
+class MyAppRow extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Demo Row'),
+      ),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green, // Warna latar belakang
+                //foregroundColor: Colors.white,
+              ),
+              child: Text('Button 1'),
+            ),
+            SizedBox(width: 10),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                //foregroundColor: Colors.white,
+              ),
+              child: Text('Button 2'),
+            ),
+            SizedBox(width: 10),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.red,
+                //foregroundColor: Colors.white,
+              ),
+              child: Text('Button 3'),
+            ),
+            SizedBox(width: 10),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.purple,
+                //foregroundColor: Colors.white,
+              ),
+              child: Text('Button 4'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
